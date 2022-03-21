8aeaddb1e84f4288e5ee392d73df78d43cd4f835
Tobias Linder <tobias@celenia.se>
2022-03-21 20:36:09 +0100

    Added combat tracker and initiative. Now adding Combatant file. Forgot that one.

---
+++ module/combat/CD10Combatant.js
@@ -0,0 +1,35 @@
+export default class CD10Combatant extends Combatant {
+  _getInitiativeFormula(combatant) {
+    let baseFormula = super._getInitiativeFormula(combatant);
+
+    const actor = game.actors.get(this.data.actorId);
+
+    let skillValue,
+      skillName,
+      modifier = actor.getModifier;
+    actor.items.forEach((w) => {
+      if (w.type === "meleeWeapon" || w.type === "rangedWeapon") {
+        if (w.data.data.isEquipped.value) {
+          actor.items.forEach((s) => {
+            if (s.type === "skill") {
+              if (s.data.data.matchID === w.data.data.attackSkill.value) {
+                skillValue = s.data.data.skillLevel.value;
+                skillName = s.name;
+              }
+            }
+          });
+        }
+      }
+    });
+
+    let actionValue = skillValue;
+
+    baseFormula = `1d10x9 + ${actionValue}`;
+    if (modifier > 0) {
+      baseFormula += ` - ${modifier}`;
+    }
+
+    console.log(baseFormula);
+    return baseFormula;
+  }
+}
